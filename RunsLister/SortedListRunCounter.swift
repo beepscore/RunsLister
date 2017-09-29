007240c7e6b624931cef02242c1c4d62f96992d0
beepscore <support@beepscore.com>
2017-09-28 17:10:58 -0700

    Add SortedListRunCounter with runCount

---
+++ RunsLister/SortedListRunCounter.swift
@@ -0,0 +1,21 @@
+//
+//  SortedListRunCounter.swift
+//  RunsLister
+//
+//  Created by Steve Baker on 9/28/17.
+//  Copyright © 2017 Beepscore LLC. All rights reserved.
+//
+
+import Foundation
+
+class SortedListRunCounter {
+
+    /// - Parameters:
+    ///   - intsSortedAscending: a list of integers sorted in increasing order
+    ///   - value: the integer to search for
+    /// - Returns: the count of occurrences of value. Returns 0 if not found.
+    class func runCount(intsSortedAscending: [Int], value: Int) -> Int {
+        return 0
+    }
+
+}
